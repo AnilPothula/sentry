d5aa17c39ac0cd149edcb5dcc4fb8e3665c80626
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-08-26 17:35:33 -0700

    WIP

--- tests/sentry/integrations/slack/test_uninstall.py
+++ tests/sentry/integrations/slack/test_uninstall.py
@@ -1,10 +1,14 @@
+from typing import Optional
+
 from sentry.models import (
     Identity,
     IdentityProvider,
     IdentityStatus,
     Integration,
     NotificationSetting,
+    Organization,
     OrganizationIntegration,
+    Project,
 )
 from sentry.notifications.helpers import _get_notification_setting_default
 from sentry.notifications.types import NotificationSettingOptionValues, NotificationSettingTypes
@@ -19,26 +23,30 @@ class SlackUninstallTest(APITestCase):
     method = "delete"
 
     def setUp(self) -> None:
-        self.integration = Integration.objects.create(
+        self.integration = self.create_integration(self.organization, "TXXXXXXX1")
+        self.login_as(self.user)
+
+    def create_integration(self, organization: Organization, external_id: str):
+        integration = Integration.objects.create(
             provider="slack",
             name="Team A",
-            external_id="TXXXXXXX1",
+            external_id=external_id,
             metadata={
                 "access_token": "xoxp-xxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxxx",
                 "installation_type": "born_as_bot",
             },
         )
-        self.integration.add_organization(self.organization)
+        integration.add_organization(organization)
 
-        self.idp = IdentityProvider.objects.create(type="slack", external_id="TXXXXXXX1", config={})
-        self.identity = Identity.objects.create(
+        idp = IdentityProvider.objects.create(type="slack", external_id=external_id, config={})
+        Identity.objects.create(
             external_id="UXXXXXXX1",
-            idp=self.idp,
+            idp=idp,
             user=self.user,
             status=IdentityStatus.VALID,
             scopes=[],
         )
-        self.login_as(self.user)
+        return integration
 
     def uninstall(self) -> None:
         assert OrganizationIntegration.objects.filter(
@@ -54,50 +62,68 @@ class SlackUninstallTest(APITestCase):
             integration=self.integration, organization=self.organization
         ).exists()
 
-    def get_notification(self, provider: ExternalProviders) -> NotificationSettingOptionValues:
+    def get_setting(
+        self, provider: ExternalProviders, project: Optional[Project] = None
+    ) -> NotificationSettingOptionValues:
         type = NotificationSettingTypes.ISSUE_ALERTS
         value = NotificationSetting.objects.get_settings(provider, type, user=self.user)
         if value != NotificationSettingOptionValues.DEFAULT:
             return value
         return _get_notification_setting_default(provider, type)
 
-    def set_notification(
+    def assert_settings(
+        self, provider: ExternalProviders, value: NotificationSettingOptionValues
+    ) -> None:
+        assert self.get_setting(provider) == value
+        assert self.get_setting(provider, project=self.project) == value
+
+    def set_setting(
         self, provider: ExternalProviders, value: NotificationSettingOptionValues
     ) -> None:
         type = NotificationSettingTypes.ISSUE_ALERTS
         NotificationSetting.objects.update_settings(provider, type, value, user=self.user)
+        NotificationSetting.objects.update_settings(
+            provider, type, value, user=self.user, project=self.project
+        )
 
     def test_uninstall_email_only(self):
         self.uninstall()
 
-        assert (
-            self.get_notification(ExternalProviders.EMAIL) == NotificationSettingOptionValues.ALWAYS
-        )
-        assert (
-            self.get_notification(ExternalProviders.SLACK) == NotificationSettingOptionValues.NEVER
-        )
+        self.assert_settings(ExternalProviders.EMAIL, NotificationSettingOptionValues.ALWAYS)
+        self.assert_settings(ExternalProviders.SLACK, NotificationSettingOptionValues.NEVER)
 
     def test_uninstall_slack_and_email(self):
-        self.set_notification(ExternalProviders.SLACK, NotificationSettingOptionValues.ALWAYS)
+        self.set_setting(ExternalProviders.SLACK, NotificationSettingOptionValues.ALWAYS)
 
         self.uninstall()
 
-        assert (
-            self.get_notification(ExternalProviders.EMAIL) == NotificationSettingOptionValues.ALWAYS
-        )
-        assert (
-            self.get_notification(ExternalProviders.SLACK) == NotificationSettingOptionValues.NEVER
-        )
+        self.assert_settings(ExternalProviders.EMAIL, NotificationSettingOptionValues.ALWAYS)
+        self.assert_settings(ExternalProviders.SLACK, NotificationSettingOptionValues.NEVER)
 
     def test_uninstall_slack_only(self):
-        self.set_notification(ExternalProviders.EMAIL, NotificationSettingOptionValues.NEVER)
-        self.set_notification(ExternalProviders.SLACK, NotificationSettingOptionValues.ALWAYS)
+        self.set_setting(ExternalProviders.EMAIL, NotificationSettingOptionValues.NEVER)
+        self.set_setting(ExternalProviders.SLACK, NotificationSettingOptionValues.ALWAYS)
 
         self.uninstall()
 
-        assert (
-            self.get_notification(ExternalProviders.EMAIL) == NotificationSettingOptionValues.NEVER
-        )
-        assert (
-            self.get_notification(ExternalProviders.SLACK) == NotificationSettingOptionValues.NEVER
-        )
+        self.assert_settings(ExternalProviders.EMAIL, NotificationSettingOptionValues.NEVER)
+        self.assert_settings(ExternalProviders.SLACK, NotificationSettingOptionValues.NEVER)
+
+    def test_uninstall_with_multiple_organizations(self):
+        organization = self.create_organization(owner=self.user)
+        integration = self.create_integration(organization, "TXXXXXXX2")
+
+        # TODO MARCOS FIRST do i even need notification settings?
+
+        self.set_setting(ExternalProviders.EMAIL, NotificationSettingOptionValues.NEVER)
+        self.set_setting(ExternalProviders.SLACK, NotificationSettingOptionValues.ALWAYS)
+
+        self.uninstall()
+
+        assert Integration.objects.filter(id=integration.id).exists()
+        assert OrganizationIntegration.objects.filter(
+            integration=integration, organization=organization
+        ).exists()
+
+        assert self.get_setting(ExternalProviders.EMAIL) == NotificationSettingOptionValues.NEVER
+        assert self.get_setting(ExternalProviders.SLACK) == NotificationSettingOptionValues.NEVER
